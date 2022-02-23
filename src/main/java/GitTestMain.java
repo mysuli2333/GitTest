7738a07733c14d9223ca3ba35416924850641f22
yuelinhu <yuelinhu@2333.com>
2022-02-23 20:31:24 +0800

    hotfix commit1

--- src/main/java/GitTestMain.java
+++ src/main/java/GitTestMain.java
@@ -2,5 +2,6 @@ public class GitTestMain {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("commit test");
+        System.out.println("hot-fix test");
     }
 }
